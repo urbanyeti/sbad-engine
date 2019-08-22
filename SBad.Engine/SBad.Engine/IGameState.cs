6c920697563dda5c5bc82b001565a3e5de64d7b6
Urbanyeti <urbanski@gmail.com>
2019-08-22 14:12:40 -0700

    Add GameState interface

---
+++ SBad.Engine/SBad.Engine/IGameState.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SBad.Visual.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SBad.Engine
+{
+    public interface IGameState
+    {
+        GraphicsDeviceManager GraphicsDeviceManager { get; set; }
+        SpriteBatch SpriteBatch { get; set; }
+        TextureFrameStore TextureFrameStore { get; set; }
+    }
+}
